b280963b70047b6fd5369f686068875ac7e86ee3
sha0coder <sha0@badchecksum.net>
2021-10-18 10:41:46 +0200

    memory redesign, a more realistic stack, a genetic memory object for code and stack or heap

--- src/emu32/mem32.rs
+++ src/emu32/mem32.rs
@@ -1,44 +1,79 @@
-use std::collections::HashMap;
+/*
+    Little endian generic memory
+*/
+
+const MAX_MEM:usize = 0x000f0000;
 
 pub struct Mem32 {
-    mem: HashMap<u8, u8>
+    base_addr: u32,
+    pub mem:[u8;MAX_MEM],
 }
 
 impl Mem32 {
     pub fn new() -> Mem32 {
         Mem32 {
-            mem: HashMap::new()
+            base_addr: 0,
+            mem:[0;MAX_MEM],
         }
     }
 
-    pub fn clear(&mut self) {
-        self.mem.clear();
-    }
-    
-    pub fn put(&mut self, key:u8, value:u8) {
-        self.mem.insert(key,value);
+    pub fn size(&self) -> usize {
+        return MAX_MEM;
     }
 
-    pub fn get(&self, key:&u8) -> u8 {
-        if self.mem.contains_key(key) {
-            return self.mem[key];
-        } else {
-            return 0;
-        }
+    pub fn set_base(&mut self, base_addr:u32) {
+        self.base_addr = base_addr;
     }
+
+    pub fn read_from(&self, addr:u32) -> &[u8] {
+        let idx = (addr - self.base_addr) as usize;
+        self.mem.get(idx..MAX_MEM).unwrap()
+    }
+
+    pub fn read_byte(&self, addr:u32) -> u8 {
+        let idx = (addr - self.base_addr) as usize;
+        self.mem[idx]
+    }
+
+    pub fn read_word(&self, addr:u32) -> u16 {
+        let idx = (addr - self.base_addr) as usize;
+        ((self.mem[idx] as u16)  << 0) + 
+        ((self.mem[idx+1] as u16) << 8)
+    }
+
+    pub fn read_dword(&self, addr:u32) -> u32 {
+        let idx = (addr - self.base_addr) as usize;
+        ((self.mem[idx] as u32)   <<  0) +
+        ((self.mem[idx+1] as u32) <<  8) +
+        ((self.mem[idx+2] as u32) << 16) +
+        ((self.mem[idx+3] as u32) << 24)
+    }
+
+    pub fn write_byte(&mut self, addr:u32, value:u8) {
+        let idx = (addr - self.base_addr) as usize;
+        self.mem[idx] = value;
+    }
+
+    pub fn write_word(&mut self, addr:u32, value:u16) {
+        let idx = (addr - self.base_addr) as usize;
+        self.mem[idx]   = (value & 0x00ff) as u8;
+        self.mem[idx+1] = ((value & 0xff00) >> 8) as u8;
+    }
+
+    pub fn write_dword(&mut self, addr:u32, value:u32) {
+        let idx = (addr - self.base_addr) as usize;
+        self.mem[idx]   = (value & 0x000000ff) as u8;
+        self.mem[idx+1] = ((value & 0x0000ff00) >> 8) as u8;
+        self.mem[idx+2] = ((value & 0x00ff0000) >> 16) as u8;
+        self.mem[idx+3] = ((value & 0xff000000) >> 24) as u8;
+    }
+
+    pub fn print(&self) {
+        println!("---mem---");
+        for b in self.mem.iter() {
+            print!("{}", b);
+        }
+        println!("");
+    }
+
 }
-
-
-
-
-/*
-
-let mem = Mem.new();
-
-mem.put(0x123, 'AAAA')
-mem.put(0x1111, 'ZZZZ')
-
-? = mem.get(0x123)
-
-*/
-
