7ec3fa5d8bc16882f66c234d61fb1f96a45aa919
sha0coder <sha0@badchecksum.net>
2021-11-10 19:30:48 +0100

    cpuid implemented, and few API

---
+++ src/emu32/constants.rs
@@ -0,0 +1,8 @@
+
+// https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/596a1078-e883-4972-9bbc-49e60bebca55
+#[allow(unused_variables)]
+pub const STATUS_SUCCESS:u32 = 0x00000000;
+pub const STATUS_ACCESS_DENIED:u32 = 0xC0000022;
+pub const STATUS_INVALID_HANDLE:u32 = 0xC0000008;
+pub const STATUS_NO_MEMORY:u32 = 0xC0000017;
+
