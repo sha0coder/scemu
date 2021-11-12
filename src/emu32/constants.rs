022afb4ce1bcafadbc26c05b5452ee5e75239d3c
sha0coder <sha0@badchecksum.net>
2021-11-12 23:35:50 +0100

    minor changes

--- src/emu32/constants.rs
+++ src/emu32/constants.rs
@@ -1,6 +1,6 @@
 
 // https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/596a1078-e883-4972-9bbc-49e60bebca55
-#[allow(unused_variables)]
+#![allow(dead_code)]
 pub const STATUS_SUCCESS:u32 = 0x00000000;
 pub const STATUS_ACCESS_DENIED:u32 = 0xC0000022;
 pub const STATUS_INVALID_HANDLE:u32 = 0xC0000008;
