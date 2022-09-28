a317c234c11d1400032f67c141b7d4fa372c6d3a
Brandon Ros <brandonros1@gmail.com>
2022-09-28 13:09:38 -0400

    fix regexp error

--- scripts/scemu-vs-x64dbg-parser.js
+++ scripts/scemu-vs-x64dbg-parser.js
@@ -43,6 +43,9 @@ const parseX64DbgRegisterChanges = (input) => {
 }
 
 const parseX64DbgMemoryChanges = (input) => {
+  if (!input) {
+    return []
+  }
   // TODO: parse more
   return [
     input
@@ -62,7 +65,7 @@ const run = async () => {
       }
     })
   const linePattern = /diff_reg: rip = ([0-9a-fA-F]+)/
-  const changesPattern = /([a-z0-9]+) ([0-9a-fA-F]+) -> ([0-9a-fA-F]+);/
+  const changesPattern = /([a-z0-9]+) ([0-9a-fA-F]+) -> ([0-9a-fA-F]+);/g
   const scemuLines = fs.readFileSync('/Users/brandonros/Desktop/scemu/scripts/scemu-output.txt').toString()
     .split('\n')
     .map(line => line.trim())
@@ -73,8 +76,8 @@ const run = async () => {
         throw new Error(`Failed to match: ${line}`)
       }
       const rip = parseInt(lineMatchResults[1], 16).toString(16)
-      const registerChangesMatchResults = changesPattern.exec(line)
-      if (!registerChangesMatchResults) {
+      const registerChangesMatchGroups = Array.from(line.matchAll(changesPattern))
+      if (!registerChangesMatchGroups || registerChangesMatchGroups.length === 0) {
         return {
           rawLine: line,
           rip,
@@ -83,12 +86,15 @@ const run = async () => {
         }
       }
       const registerChanges = []
-      for (let i = 1; i < registerChangesMatchResults.length; i += 3) {
-        registerChanges.push({
-          registerName: registerChangesMatchResults[i],
-          previousValue: BigInt(`0x${registerChangesMatchResults[i + 1]}`).toString(16),
-          newValue: BigInt(`0x${registerChangesMatchResults[i + 2]}`).toString(16),
-        })
+      for (let i = 0; i < registerChangesMatchGroups.length; ++i) {
+        const registerChangesMatchGroup = registerChangesMatchGroups[i]
+        for (let x = 1; x < registerChangesMatchGroup.length; x += 3) {
+          registerChanges.push({
+            registerName: registerChangesMatchGroup[x],
+            previousValue: BigInt(`0x${registerChangesMatchGroup[x + 1]}`).toString(16),
+            newValue: BigInt(`0x${registerChangesMatchGroup[x + 2]}`).toString(16),
+          })
+        }
       }
       registerChanges.sort((a, b) => {
         if (a.registerName < b.registerName) {
@@ -99,6 +105,7 @@ const run = async () => {
         return 0
       })
       return {
+        rawLine: line,
         rip,
         registerChanges,
         memoryChanges: [
@@ -129,29 +136,61 @@ const run = async () => {
       fs.writeFileSync('/Users/brandonros/Desktop/scemu/scripts/scemu-errors.json', JSON.stringify(errors, undefined, 2))
       throw new Error('rip mismatch')
     }
-    // number of register change mismatches
-    if (x64dbgLine.registerChanges.length !== scemuLine.registerChanges.length) {
-      instructionErrors.push({
-        message: 'number of register changes mismatch'
-      })
-      errors.push({
-        i,
-        x64dbgLine,
-        scemuLine,
-        instructionErrors
-      })
-      continue
-    }
-    // register change mismatches
+    // register change mismatches (x64dbg)
     for (let x = 0; x < x64dbgLine.registerChanges.length; ++x) {
-      if (x64dbgLine.registerChanges[x].registerName !== scemuLine.registerChanges[x].registerName) {
+      const x64dbgRegisterChange = x64dbgLine.registerChanges[x]
+      const scemuRegisterChange = scemuLine.registerChanges.find(scemuRegisterChange => scemuRegisterChange.registerName === x64dbgRegisterChange.registerName)
+      if (scemuRegisterChange) {
+        if (x64dbgRegisterChange.previousValue !== scemuRegisterChange.previousValue) {
+          instructionErrors.push({
+            index: x,
+            message: 'previousValue mismatch',
+            x64dbg: x64dbgRegisterChange.previousValue,
+            scemu: scemuRegisterChange.previousValue
+          })
+        }
+        if (x64dbgRegisterChange.newValue !== scemuRegisterChange.newValue) {
+          instructionErrors.push({
+            index: x,
+            message: 'newValue mismatch',
+            x64dbg: x64dbgRegisterChange.previousValue,
+            scemu: scemuRegisterChange.previousValue
+          })
+        }
+      } else {
         instructionErrors.push({
-          message: 'registerName mismatch'
+          index: x,
+          message: 'unmatchedRegisterChange mismatch (x64dbg but not scemu)',
+          x64dbg: x64dbgRegisterChange.registerName
         })
       }
-      if (x64dbgLine.registerChanges[x].newValue !== scemuLine.registerChanges[x].newValue) {
+    }
+    // register change mismatches (scemu)
+    for (let x = 0; x < scemuLine.registerChanges.length; ++x) {
+      const scemuRegisterChange = scemuLine.registerChanges[x]
+      const x64dbgRegisterChange = x64dbgLine.registerChanges.find(x64dbgRegisterChange => scemuRegisterChange.registerName === x64dbgRegisterChange.registerName)
+      if (x64dbgRegisterChange) {
+        if (x64dbgRegisterChange.previousValue !== scemuRegisterChange.previousValue) {
+          instructionErrors.push({
+            index: x,
+            message: 'previousValue mismatch',
+            x64dbg: x64dbgRegisterChange.previousValue,
+            scemu: scemuRegisterChange.previousValue
+          })
+        }
+        if (x64dbgRegisterChange.newValue !== scemuRegisterChange.newValue) {
+          instructionErrors.push({
+            index: x,
+            message: 'newValue mismatch',
+            x64dbg: x64dbgRegisterChange.previousValue,
+            scemu: scemuRegisterChange.previousValue
+          })
+        }
+      } else {
         instructionErrors.push({
-          message: 'newValue mismatch'
+          index: x,
+          message: 'unmatchedRegisterChange mismatch (scemu but not x64dbg)',
+          scemu: scemuRegisterChange.registerName
         })
       }
     }
