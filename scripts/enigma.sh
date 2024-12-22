99b10a60b34ab2fc1b0f5ddb5b2b201f8c2d43f4
Brandon Ros <brandonros1@gmail.com>
2024-12-22 13:34:46 -0500

    tweak script path

--- scripts/enigma.sh
+++ scripts/enigma.sh
@@ -8,7 +8,7 @@ cargo run --target x86_64-apple-darwin \
     -- \
     -vv \
     --64bits \
-    --maps scemu/maps64 \
+    --maps ./maps64 \
     --trace /tmp/output.csv \
     --memory \
     --filename ~/Desktop/enigma/surprise.dll
