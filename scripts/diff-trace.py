b3475f9912ee38a7e8c6ca1b27e8fe3564b9cfd4
Brandon Ros <brandonros1@gmail.com>
2024-12-25 16:46:54 -0500

    simplify diff trace script

--- scripts/diff-trace.py
+++ scripts/diff-trace.py
@@ -2,8 +2,8 @@
 
 import csv
 
-TRACE1_PATH = '/tmp/output.csv' # scemu
-TRACE2_PATH = '/Users/brandon/Downloads/export-20241222-171939.csv' # x64dbg
+SCEMU_TRACE_PATH = '/tmp/output.csv' # scemu
+X64DBG_TRACE_PATH = '/Users/brandon/Downloads/export-20241222-171939.csv' # x64dbg
 EXPECTED_HEADERS = ["Index", "Address", "Bytes", "Disassembly", "Registers", "Memory", "Comments"]
 EXPECTED_BASE = 0x180000000
 EXPECTED_ENTRY = 0x181035FF0
@@ -26,73 +26,65 @@ def calculate_offset(first_addr):
 
 def compare_traces():
     print(f"Opening files:")
-    print(f"Trace 1: {TRACE1_PATH}")
-    print(f"Trace 2: {TRACE2_PATH}")
+    print(f"scemu trace: {SCEMU_TRACE_PATH}")
+    print(f"x64dbg trace: {X64DBG_TRACE_PATH}")
     
-    with open(TRACE1_PATH, 'r') as f1, open(TRACE2_PATH, 'r') as f2:
+    with open(SCEMU_TRACE_PATH, 'r') as f_scemu, open(X64DBG_TRACE_PATH, 'r') as f_x64dbg:
         print("Files opened successfully")
         
-        reader1 = csv.DictReader(f1)
-        reader2 = csv.DictReader(f2)
+        scemu_reader = csv.DictReader(f_scemu)
+        x64dbg_reader = csv.DictReader(f_x64dbg)
         
         # Get first row to calculate offsets
-        row1 = next(reader1)
-        row2 = next(reader2)
+        scemu_row = next(scemu_reader)
+        x64dbg_row = next(x64dbg_reader)
         
         # Calculate offsets based on first address
-        offset1 = calculate_offset(parse_hex(row1['Address'].split()[0]))
-        offset2 = calculate_offset(parse_hex(row2['Address'].split()[0]))
+        scemu_offset = calculate_offset(parse_hex(scemu_row['Address'].split()[0]))
+        x64dbg_offset = calculate_offset(parse_hex(x64dbg_row['Address'].split()[0]))
         
-        print(f"Trace 1 offset: 0x{offset1:x}")
-        print(f"Trace 2 offset: 0x{offset2:x}")
-
-        # Compare first row
-        idx1 = parse_hex(row1['Index'])
-        idx2 = parse_hex(row2['Index'])
-        addr1 = parse_hex(row1['Address'].split()[0]) - offset1
-        addr2 = parse_hex(row2['Address'].split()[0]) - offset2
-
-        if idx1 != idx2 or addr1 != addr2:
-            print(f"Difference found in first row:")
-            print(f"Trace 1: Index=0x{idx1:x}, RVA=0x{addr1:x}")
-            print(f"Trace 2: Index=0x{idx2:x}, RVA=0x{addr2:x}")
-            return
+        print(f"scemu trace offset: 0x{scemu_offset:x}")
+        print(f"x64dbg trace offset: 0x{x64dbg_offset:x}")
 
         # Add buffer for previous lines
-        prev_lines1 = []
-        prev_lines2 = []
+        scemu_prev_lines = [(parse_hex(scemu_row['Index']), 
+                        parse_hex(scemu_row['Address'].split()[0]) - scemu_offset, 
+                        scemu_row)]
+        x64dbg_prev_lines = [(parse_hex(x64dbg_row['Index']), 
+                        parse_hex(x64dbg_row['Address'].split()[0]) - x64dbg_offset, 
+                        x64dbg_row)]
         max_history = 4
 
-        # Compare remaining rows
-        for row_num, (row1, row2) in enumerate(zip(reader1, reader2), start=2):
-            idx1 = parse_hex(row1['Index'])
-            idx2 = parse_hex(row2['Index'])
-            addr1 = parse_hex(row1['Address'].split()[0]) - offset1
-            addr2 = parse_hex(row2['Address'].split()[0]) - offset2
+        # Compare all rows
+        for row_num, (scemu_row, x64dbg_row) in enumerate(zip(scemu_reader, x64dbg_reader), start=2):
+            scemu_idx = parse_hex(scemu_row['Index'])
+            x64dbg_idx = parse_hex(x64dbg_row['Index'])
+            scemu_addr = parse_hex(scemu_row['Address'].split()[0]) - scemu_offset
+            x64dbg_addr = parse_hex(x64dbg_row['Address'].split()[0]) - x64dbg_offset
 
             # Store current line in history
-            prev_lines1.append((idx1, addr1, row1))
-            prev_lines2.append((idx2, addr2, row2))
-            if len(prev_lines1) > max_history:
-                prev_lines1.pop(0)
-                prev_lines2.pop(0)
+            scemu_prev_lines.append((scemu_idx, scemu_addr, scemu_row))
+            x64dbg_prev_lines.append((x64dbg_idx, x64dbg_addr, x64dbg_row))
+            if len(scemu_prev_lines) > max_history:
+                scemu_prev_lines.pop(0)
+                x64dbg_prev_lines.pop(0)
 
-            if idx1 != idx2 or addr1 != addr2:
+            if scemu_idx != x64dbg_idx or scemu_addr != x64dbg_addr:
                 print(f"\nDifference found at row {row_num}:")
-                print("\nPrevious 4 lines from Trace 1:")
-                for prev_idx, prev_addr, prev_row in prev_lines1:
+                print("\nPrevious 4 lines from scemu trace:")
+                for prev_idx, prev_addr, prev_row in scemu_prev_lines:
                     print(f"Index=0x{prev_idx:x}, RVA=0x{prev_addr:x}")
                     print(f"Full row: {prev_row}")
-                print("\nPrevious 4 lines from Trace 2:")
-                for prev_idx, prev_addr, prev_row in prev_lines2:
+                print("\nPrevious 4 lines from x64dbg trace:")
+                for prev_idx, prev_addr, prev_row in x64dbg_prev_lines:
                     print(f"Index=0x{prev_idx:x}, RVA=0x{prev_addr:x}")
                     print(f"Full row: {prev_row}")
                 print("\nDifferent lines:")
-                print(f"Trace 1: Index=0x{idx1:x}, RVA=0x{addr1:x}")
-                print(f"Trace 2: Index=0x{idx2:x}, RVA=0x{addr2:x}")
+                print(f"scemu trace: Index=0x{scemu_idx:x}, RVA=0x{scemu_addr:x}")
+                print(f"x64dbg trace: Index=0x{x64dbg_idx:x}, RVA=0x{x64dbg_addr:x}")
                 print("\nRaw values:")
-                print(f"Trace 1: {row1}")
-                print(f"Trace 2: {row2}")
+                print(f"scemu trace: {scemu_row}")
+                print(f"x64dbg trace: {x64dbg_row}")
                 return
 
 if __name__ == '__main__':
