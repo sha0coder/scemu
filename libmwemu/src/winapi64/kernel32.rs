70c250503302757fa59e3670c4bbd5e35d1dcd09
Brandon Ros <brandonros1@gmail.com>
2024-12-28 17:17:44 -0500

    more kernel32

--- libmwemu/src/winapi64/kernel32.rs
+++ libmwemu/src/winapi64/kernel32.rs
@@ -158,6 +158,8 @@ pub fn gateway(addr: u64, emu: &mut emu::Emu) -> String {
         "GetConsoleOutputCP" => GetConsoleOutputCP(emu),
         "GetCommandLineA" => GetCommandLineA(emu),
         "GetCommandLineW" => GetCommandLineW(emu),
+        "GetCPInfo" => GetCPInfo(emu),
+        "GetUserDefaultLCID" => GetUserDefaultLCID(emu),
 
         _ => {
             unimplemented!(
@@ -3017,4 +3019,61 @@ fn GetCommandLineW(emu: &mut emu::Emu) {
     let cmdline = emu.alloc("cmdline", 1024);
     emu.maps.write_wide_string(cmdline, "test.exe");
     emu.regs.rax = cmdline;
+}
+
+/*
+BOOL GetCPInfo(
+  [in]  UINT     CodePage,
+  [out] LPCPINFO lpCPInfo
+);
+*/
+fn GetCPInfo(emu: &mut emu::Emu) {
+    let code_page = emu.regs.rcx as usize;
+    let lp_cp_info = emu.regs.rdx;
+
+    log::info!(
+        "{}** {} kernel32!GetCPInfo code_page: {} lp_cp_info: 0x{:x} {}",
+        emu.colors.light_red,
+        emu.pos,
+        code_page,
+        lp_cp_info,
+        emu.colors.nc
+    );
+
+    // Create and initialize a CpInfo structure
+    let cp_info = match code_page {
+        // CP_ACP (0) - ANSI code page
+        0 => structures::CpInfo {
+            max_char_size: 2,
+            default_char: [0x3F, 0], // '?'
+            lead_byte: [0; 12],
+        },
+        // CP_OEMCP (1) - OEM code page
+        1 => structures::CpInfo {
+            max_char_size: 1,
+            default_char: [0x3F, 0], // '?'
+            lead_byte: [0; 12],
+        },
+        // For other code pages, use default values
+        _ => structures::CpInfo::new(),
+    };
+
+    // Save the CpInfo structure to the provided memory location
+    cp_info.save(lp_cp_info, &mut emu.maps);
+
+    // Return TRUE to indicate success
+    emu.regs.rax = 1;
+}
+
+/*
+DWORD GetUserDefaultLCID();
+*/
+fn GetUserDefaultLCID(emu: &mut emu::Emu) {
+    log::info!(
+        "{}** {} kernel32!GetUserDefaultLCID {}",
+        emu.colors.light_red,
+        emu.pos,
+        emu.colors.nc
+    );
+    emu.regs.rax = 0x00000400;
 }
