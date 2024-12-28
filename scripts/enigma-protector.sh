fcc7e37acef0efca96bb0381345e789b052649d6
Brandon Ros <brandonros1@gmail.com>
2024-12-28 14:09:00 -0500

    cross script

--- scripts/enigma-protector.sh
+++ scripts/enigma-protector.sh
@@ -5,8 +5,15 @@ set -x
 export RUST_BACKTRACE=1
 export RUST_LOG=info
 
+# Set target architecture based on OS
+if [[ "$OSTYPE" == "msys"* ]] || [[ "$OSTYPE" == "cygwin"* ]]; then
+    TARGET=x86_64-pc-windows-msvc
+else
+    TARGET=x86_64-apple-darwin
+fi
+
 cargo run -p mwemu --release \
-    --target x86_64-apple-darwin \
+    --target $TARGET \
     -- \
     --filename ~/Desktop/enigma/surprise.dll \
     --maps ./maps64/ \
