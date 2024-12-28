822a55d5bdc94cc3330e0ff41831c78422c8f35e
sha0coder <sha0@badchecksum.net>
2024-12-28 13:36:58 +0100

    get module handle update

--- libmwemu/src/emu/winapi64/kernel32.rs
+++ libmwemu/src/emu/winapi64/kernel32.rs
@@ -2776,35 +2776,59 @@ fn GetModuleHandleA(emu: &mut emu::Emu) {
             Some(base) => base,
             None => helper::handler_create(&module_name),
         }
-    } else {
-        let module_name = emu.maps.read_string(module_name_ptr);
-        let handle = get_library_handle(emu, &module_name);
 
-        log::info!(
-            "{}** {} kernel32!GetModuleHandleA module_name: {} handle: 0x{:x} {}",
-            emu.colors.light_red,
-            emu.pos,
-            module_name,
-            handle,
-            emu.colors.nc
-        );
+    } else {
+        module_name = emu.maps.read_string(module_name_ptr).to_lowercase();
+        let mod_mem = match emu.maps.get_mem2(&module_name) {
+            Some(m) => m,
+            None => {
+                emu.regs.rax = 0;
+                return;
+            }
+        };
+
+        emu.regs.rax = mod_mem.get_base();
     }
 
-    emu.regs.rax = handle;
-}
-
-fn GetModuleHandleW(emu: &mut emu::Emu) {
-    let module_name = emu.maps.read_wide_string(emu.regs.rcx);
-    let handle = get_library_handle(emu, &module_name);
-
     log::info!(
-        "{}** {} kernel32!GetModuleHandleW module_name: {} handle: 0x{:x} {}",
+        "{}** {} kernel32!GetModuleHandleA `{}`  {}",
         emu.colors.light_red,
         emu.pos,
         module_name,
-        handle,
         emu.colors.nc
     );
 
-    emu.regs.rax = handle;
+}
+
+fn GetModuleHandleW(emu: &mut emu::Emu) {
+    let module_name_ptr = emu.regs.rcx;
+    let module_name: String;
+
+    if module_name_ptr == 0 {
+        module_name = "self".to_string();
+        emu.regs.rax = match emu.maps.get_base() {
+            Some(base) => base,
+            None => helper::handler_create(&module_name),
+        }
+
+    } else {
+        module_name = emu.maps.read_wide_string(module_name_ptr).to_lowercase();
+        let mod_mem = match emu.maps.get_mem2(&module_name) {
+            Some(m) => m,
+            None => {
+                emu.regs.rax = 0;
+                return;
+            }
+        };
+
+        emu.regs.rax = mod_mem.get_base();
+    }
+
+    log::info!(
+        "{}** {} kernel32!GetModuleHandleW `{}`  {}",
+        emu.colors.light_red,
+        emu.pos,
+        module_name,
+        emu.colors.nc
+    );
 }
