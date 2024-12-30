7faa273766111d62d281ce4c0b0bffaa1ed5d752
Brandon Ros <brandonros1@gmail.com>
2024-12-29 23:22:16 -0500

    CreateFontIndirectA?

--- libmwemu/src/winapi64/gdi32.rs
+++ libmwemu/src/winapi64/gdi32.rs
@@ -5,6 +5,7 @@ use crate::winapi64;
 pub fn gateway(addr: u64, emu: &mut emu::Emu) -> String {
     let api = winapi64::kernel32::guess_api_name(emu, addr);
     match api.as_str() {
+        "CreateFontIndirectA" => CreateFontIndirectA(emu),
         _ => {
             if emu.cfg.skip_unimplemented == false {
                 if emu.cfg.dump_on_exit && emu.cfg.dump_filename.is_some() {
@@ -18,4 +19,16 @@ pub fn gateway(addr: u64, emu: &mut emu::Emu) -> String {
         }
     }
     String::new()
+}
+
+/*
+HFONT CreateFontIndirectA(
+  [in] const LOGFONTA *lplf
+);
+*/
+fn CreateFontIndirectA(emu: &mut emu::Emu) {
+    log_red!(emu, "** {} gdi32!CreateFontIndirectA", emu.pos);
+    // TODO: return a handle to a logical font?
+    // TODO: don't return failure
+    emu.regs.rax = 0;
 }
